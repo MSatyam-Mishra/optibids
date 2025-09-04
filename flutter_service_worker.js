036030ffbd278bca97800b28bc2acff183737526
MSatyam-Mishra <satyam12295@gmail.com>
2025-09-04 18:54:14 +0530

    commit

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -398,7 +398,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "31f2ecc5a14916478430e5e88542f280
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "da0e74d59e6b2a09a6618022892af1bd",
+"flutter_bootstrap.js": "d1d33ae4d958ae9350b50b87b5489110",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
